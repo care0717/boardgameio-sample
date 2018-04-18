d6560720da22d7171ebcfabf1b8fd6d66e1c629d
care0717 <dev.care0717@gmail.com>
2018-04-18 16:06:37 +0900

    feat: multiplayer対応

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './game';
 import { TicTacToeBoard } from './board';
+import React, { Component }  from 'react';
 
 export const WIDE = 5
 export const HIGHT = 7
 
-const App = Client({ game: TicTacToe,  board: TicTacToeBoard, });
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: { server: '192.168.1.215:8000' },
+  debug: false,
+});
+
+const App = () => (
+  <div>
+    <TicTacToeClient playerID="0" />
+    <TicTacToeClient playerID="1" />
+  </div>
+);
 
 export default App;
